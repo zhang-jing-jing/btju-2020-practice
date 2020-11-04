45d9edad7ac8372236fb0bf1bce1838ee13093cc
zhangjingjing <zhangjing@cnipr.com>
2020-11-04 20:09:33 +0800

    encode

--- transformer/main.py
+++ transformer/main.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import math, copy, time
 
+print('jia')
 #EncodeDecoder
 class EncoderDecoder(nn.Module):
     """
@@ -30,5 +31,140 @@ class EncoderDecoder(nn.Module):
     def decode(self, memory,  src_mask, tgt, tgt_mask):
         return self.decoder(self.tgt_embed(tgt), memory, src_mask, tgt_mask)
 
+class Generator(nn.Module):
+    """
+    generator则是根据解码器当前时刻的隐状态输出当前时刻的词
+    Define standard linear + softmax generation step.
+    """
+    def __init__(self, d_model, vocab):
+        super(Generator, self).__init__()
+        self.proj = nn.Linear(d_model, vocab)
+    
+    def forward(self, x):
+        return F.log_softmax(self.proj(x), dim=-1)
+
+"""
+Transformer模型也遵循着Encoder-Decoder的架构。
+它的Encoder是由N=6个相同的EncoderLayer组成，每个EncoderLayer包含一个Self-Attention Sublayer层和一个Feed-Forward Sublayer层；
+而它的Decoder也是由N=6个相同的DecoderLayer组成，每个DecoderLayer包含一个Self-Attention Sublayer层、一个Encoder-Decoder-Attention Sublayer层和一个Feed-Forward Sublayer层。
+"""
+
+def clones(module, N):
+    """
+    产生n个完全相同的层
+    """
+    return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
+
+class Encoder(nn.Module):
+    """
+    core encoder is a stack of N layers
+    """
+    def __init__(self, layer, N):
+        super(Encoder,self).__init__()
+        self.layers = clones(layer, N)
+        self.norm = LayerNorm(layer.size)
+    
+    def  forward(self, x, mask):
+        """
+        Pass the input (and mask) through each layer in turn.
+        """
+        for layer in self.layers:
+            x = layer(x, mask)
+        return self.norm(x)
+
+
+class LayerNorm(nn.Module):
+    """
+     Construct a layernorm module, see https://arxiv.org/abs/1607.06450 for details.
+    """
+    def __init__(self, features, eps=1e-6):
+        super(LayerNorm, self).__init__()
+        self.a_2 = nn.Parameter(torch.ones(features))
+        self.b_2 = nn.Parameter(torch.zeros(features))
+        self.eps = eps
+    
+    def forward(self, x):
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(-1, keepdim=True)
+        return self.a_2*(x - mean)/(std + self.eps) + self.b_2
+
+
+class SublayerConnection(nn.Module):
+    """
+    A residual connection followed by a layer norm.
+    Note for code simplicity the norm is first as opposed to last.
+    """
+    
+    def __init__(self, size, dropout):
+        super(SublayerConnection, self).__init__()
+        self.norm = LayerNorm(size)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, sublayer):
+        """
+        Apply residual connection to any sublayer with the same size
+        """
+        return x + self.dropout(sublayer(self.norm(x)))
+
+
+class EncoderLayer(nn.Module):
+    """
+    Encoder is made up of self-attn and feed forward.
+    """
+
+    def __init__(self, size, self_attn, feed_forward, dropout):
+        super(EncoderLayer, self).__init__()
+        self.self_attn = self_attn
+        self.feed_forward = feed_forward
+        self.sublayer = clones(SublayerConnection(size, dropout), 2)
+        self.size = size
+    
+    def forward(self, x, mask):
+        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, mask))
+        return self.sublayer[1](x, self.feed_forward)
+
+class Decoder(nn.Module):
+    """
+    Generic N layer decoder with masking.
+    """
+
+    def __init__(self, layer, N):
+        super(Decoder, self).__init__()
+        self.layers = clones(layer,N)
+        self.norm = LayerNorm(layer.size)
+
+    def forward(self, x, memory, src_mask, tgt_mask):
+        for layer in self.layers:
+            x = layer(x, memory, src_mask, tgt_mask)
+        return self.norm(x)
+
+
+class DecoderLayer(nn.Module):
+    """
+    Decoder is made of self-attn, src-attn, and feed forward.
+    """
+
+    def __init__(self, size, self_attn, src_attn, feed_forward, dropout):
+        super(DecoderLayer, self).__init__()
+        self.size = size
+        self.self_attn = self_attn
+        self.src_attn = src_attn
+        self.feed_forward = feed_forward
+        self.sublayer = clones(SublayerConnection(size, dropout), 3)
+
+    def forward(self, x, memory, src_mask, tgt_mask):
+        m = memory
+        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, tgt_mask))
+        x = self.sublayer[1](x, lambda x: self.src_attn(x, m, m, src_mask))
+        return self.sublayer[2](x, self.feed_forward)
+
+
+def subsequent_mask(size):
+    """
+    Mask out subsequent positions.
+    """
+    attn_shape = (1, size, size)
+    subsequent_mask = np.triu(np.ones(attn_shape), k=1).astype('uint8')
+    return torch.from_numpy(subsequent_mask) == 0
 
 
